a42451ba4be485c41196c46678d386eec6863268
harbocasper <sofieharbojensen@gmail.com>
2024-08-08 10:47:59 +0200

    added katapult for monster tower
    added ammo counter tooltip to show number and boxes the gets removed when under 10 ammo.
    also made a check for clicking on things (not done)
    added a display that holds the question for the player and shows it.

---
+++ Assets/Scenes/MonterTower katapult/scripts/AmmoPupUp.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AmmoPupUp : MonoBehaviour
+{
+
+    [SerializeField] TextMeshProUGUI textFeld;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    /// <summary>
+    /// sets the tooltip and shows it
+    /// </summary>
+    /// <param name="text"></param>
+    public void SetAndShowToolTip(string text)
+    {
+        textFeld.text = text;
+        gameObject.SetActive(true);
+    }
+
+    /// <summary>
+    /// hides the tooltip
+    /// </summary>
+    public void HideToolTip()
+    {
+        gameObject.SetActive(false);
+    }
+}
