cf5d3719fe4c53078c77b3c19f1233db80c24fa1
theAshamedsleeper <thorbjorn114@gmail.com>
2024-08-14 15:27:22 +0200

    more working on the slider functions

--- Assets/Scenes/VideoPlayer/Scripts/VideoTesting.cs
+++ Assets/Scenes/VideoPlayer/Scripts/VideoTesting.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Video;
@@ -9,6 +10,9 @@ public class VideoTesting : MonoBehaviour
     public VideoPlayer vid;
     [SerializeField] private Slider lengthSlider;
 
+
+    // Flag to check if the user is dragging the slider
+    private bool isDraggingSlider = false;
     private void Start()
     {
         lengthSlider.maxValue = (float)vid.clip.length;
@@ -16,13 +20,16 @@ public class VideoTesting : MonoBehaviour
 
     private void Update()
     {
-       lengthSlider.value = (float)vid.time;
+        // Only update the slider if the user is not dragging it
+        if (!isDraggingSlider)
+        {
+            lengthSlider.value = (float)vid.time;
+        }
 
         if (vid.time >= vid.clip.length)
         {
             Debug.Log("The end of the video has ben reached, do something different");
         }
-
     }
     public void SkipTime(float amount)
     {
@@ -35,9 +42,25 @@ public class VideoTesting : MonoBehaviour
         vid.clip = clip;
         lengthSlider.maxValue = (float)vid.clip.length;
     }
+    // Method to be called when the user starts dragging the slider
+    public void OnSliderBeginDrag()
+    {
+        isDraggingSlider = true;
+    }
+    // Method to be called when the user ends dragging the slider
+    public void OnSliderEndDrag()
+    {
+        isDraggingSlider = false;
+        vid.time = lengthSlider.value; // Set the video time to the slider value after dragging ends
+    }
+
     public void OnSlideValueChanged()
     {
-        //vid.time = lengthSlider.value;
+        // This method is now only responsible for handling value changes when the user drags the slider
+        if (isDraggingSlider)
+        {
+            vid.time = lengthSlider.value;
+        }
     }
     /*
      * vid.time | current time of video in seconds
