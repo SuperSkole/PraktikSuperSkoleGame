0386882f52e3dc0046394efd343bc3ace13befc5
ewrefads <gdonovan@live.dk>
2024-08-15 09:18:56 +0200

    Added a target marker to the monster throwing the player and misc scene changes

---
+++ Assets/Scenes/Grovæder/Scripts/DestroyOnPlayerContact.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+
+/// <summary>
+/// Used to destroy a gameobject on contact with the player
+/// </summary>
+public class DestroyOnContact : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Destroys the object when it overlaps with the player
+    /// </summary>
+    /// <param name="other"></param>
+    public void OnTriggerEnter(Collider other){
+        if(other.gameObject.tag == "Player"){
+            Destroy(gameObject);
+        }
+    }
+}
