99136fce8de38bd5c1fc168314c2db4f3dc3cb32
jonathanlillelund <jonathanlillelund@hotmail.com>
2024-08-15 23:25:44 +0200

    graphic shop UI

--- Assets/Spine/Spine stuff/Shop option.cs
+++ Assets/Spine/Spine stuff/Shop option.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -7,16 +8,21 @@ using UnityEngine.UI;
 public class Shopoption : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] string itemName;
+    [SerializeField] string Price;
+    [SerializeField] TextMeshProUGUI priceText;
 
     private ShopManager shopManager;
 
     private void Awake()
     {
         shopManager = FindObjectOfType<ShopManager>();
+
+        priceText.text = Price;
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
+        //skal lige overfører pris også
         shopManager.Click(itemName);
     }
 
