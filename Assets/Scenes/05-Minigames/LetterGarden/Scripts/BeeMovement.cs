0a86ade15b436d2228075d5ee9989665ae3c2d5a
TGW <mc-casper@hotmail.dk>
2024-08-21 13:43:54 +0200

    Updates bee movement code

--- Assets/Scenes/05-Minigames/LetterGarden/Scripts/BeeMovement.cs
+++ Assets/Scenes/05-Minigames/LetterGarden/Scripts/BeeMovement.cs
@@ -8,8 +8,11 @@ namespace Scenes.Minigames.LetterGarden.Scrips
 {
     public class BeeMovement : MonoBehaviour
     {
-        [SerializeField] private SplineContainer letterSpline;
         [SerializeField] private float speed = 1;
+        [SerializeField] private GameObject SplineParent;
+        
+        private SplineContainer letterSpline;
+        private List<SplineContainer> letterList;
 
         private Vector3 currentPos;
         private Vector3 direction;
@@ -17,17 +20,28 @@ namespace Scenes.Minigames.LetterGarden.Scrips
         private float spineLeangth;
         private int splineIndex = 0;
 
-
+        /// <summary>
+        /// Runs at start and dynamically fetches all splines used to draw letters/symbols, then selects one at random.
+        /// </summary>
         void Start()
         {
+            letterList = new();
+            foreach (Transform spline in SplineParent.GetComponentInChildren<Transform>())
+            {
+                letterList.Add(spline.gameObject.GetComponent<SplineContainer>());
+            }
+            letterSpline = letterList[Random.Range(0, letterList.Count)];
             spineLeangth = letterSpline.CalculateLength(splineIndex);
-            
         }
 
 
         void Update()
         {
-            if (letterSpline != null) MoveOnSpline();
+            if (letterSpline != null)
+            {
+                MoveOnSpline();
+                CheckDistance();
+            }
         }
 
         /// <summary>
@@ -36,12 +50,29 @@ namespace Scenes.Minigames.LetterGarden.Scrips
         /// <returns>returns false if you cant go to the next line(we are out of lines) and returns true if it sucsesfully moves on to the next line</returns>
         public bool NextSplineInLetter()
         {
-            if(splineIndex >= letterSpline.Splines.Count - 1) 
-                return false;
-            splineIndex++;
+            if(splineIndex >= letterSpline.Splines.Count - 1)
+            {
+                splineIndex = 0;
+            }
+            else
+                splineIndex++;
             distancePercentage = 0;
             spineLeangth = letterSpline.CalculateLength(splineIndex);
-            return true;
+            if(splineIndex == 0)
+                return false;
+            else
+                return true;
+        }
+
+        /// <summary>
+        /// Checks how close to completion of the current path the bee is.
+        /// </summary>
+        private void CheckDistance()
+        {
+            if (distancePercentage >= 1)
+            {
+                NextSplineInLetter();
+            }
         }
 
         /// <summary>
@@ -53,7 +84,6 @@ namespace Scenes.Minigames.LetterGarden.Scrips
 
             currentPos = letterSpline.EvaluatePosition(splineIndex,distancePercentage);
             transform.position = currentPos;
-            if(distancePercentage >= 1) distancePercentage = 0;
 
             direction = (Vector3)letterSpline.EvaluatePosition(splineIndex, distancePercentage + 0.05f) - currentPos;
             transform.rotation = Quaternion.LookRotation(direction,Vector3.back);
