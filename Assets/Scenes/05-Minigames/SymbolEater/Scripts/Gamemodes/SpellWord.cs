7db4b2cffef96d79fbb54f66f06bbd78a4065247
Marc7451 <Marc7451@gmail.com>
2024-08-21 13:42:16 +0200

    l

--- Assets/Scenes/05-Minigames/SymbolEater/Scripts/Gamemodes/SpellWord.cs
+++ Assets/Scenes/05-Minigames/SymbolEater/Scripts/Gamemodes/SpellWord.cs
@@ -57,53 +57,56 @@ namespace Scenes.Minigames.SymbolEater.Scripts.Gamemodes
         /// </summary>
         BoardController boardController;
 
-    /// <summary>
-    /// Gets the letters for the current game
-    /// </summary>
-    public void GetSymbols()
-    {
-        //Sets up variablese and checks if data is loaded
-        currentIndex = 0;
-        word = "";
-        
-        //Checks if data has been loaded and if it has it begins preparing the board. Otherwise it waits on data being loaded before restarting
-        if(DataLoader.IsDataLoaded)
+        /// <summary>
+        /// Gets the letters for the current game
+        /// </summary>
+        public void GetSymbols()
         {
-            
-            word = WordsForImagesManager.GetRandomWordForImage();
-            if(sprites.ContainsKey(word)){
-                boardController.SetImage(sprites[word]);
-            }
-            else{
-                Texture2D texture = ImageManager.GetImageFromWord(word);
-                sprites.Add(word, Sprite.Create(texture, new Rect(0.0f, 0.0f, texture.width, texture.height), new Vector2(0.5f, 0.5f), 100.0f));
-                boardController.SetImage(sprites[word]);
-            }
-            currentLetter = word[currentIndex];
-            wordsLoaded = true;
-        }
-        else 
-        {
-            boardController.StartImageWait(GetSymbols);
-        }
-        //If the words are loaded then it starts generating the board
-        if(wordsLoaded){
-            //deactives all current active lettercubes
-            foreach (LetterCube lC in activeLetterCubes)
+            //Sets up variablese and checks if data is loaded
+            currentIndex = 0;
+            word = "";
+
+            //Checks if data has been loaded and if it has it begins preparing the board. Otherwise it waits on data being loaded before restarting
+            if (DataLoader.IsDataLoaded)
             {
-                lC.Deactivate();
-            }
-            int count = Random.Range(minWrongLetters, maxWrongLetters);
-            activeLetterCubes.Clear();
-            //finds new letterboxes to be activated and assigns them a random incorrect letter.
-            for (int i = 0; i < count; i++)
-            {
-                char letter = LetterManager.GetRandomLetters(1)[0];
-                while(word.Contains(char.ToLower(letter)))
+
+                word = WordsForImagesManager.GetRandomWordForImage();
+                if (sprites.ContainsKey(word))
                 {
-                    letter = LetterManager.GetRandomLetters(1)[0];
+                    boardController.SetImage(sprites[word]);
                 }
-                LetterCube potentialCube = letterCubes[Random.Range(0, letterCubes.Count)];
+                else
+                {
+                    Texture2D texture = ImageManager.GetImageFromWord(word);
+                    sprites.Add(word, Sprite.Create(texture, new Rect(0.0f, 0.0f, texture.width, texture.height), new Vector2(0.5f, 0.5f), 100.0f));
+                    boardController.SetImage(sprites[word]);
+                }
+                currentLetter = word[currentIndex];
+                wordsLoaded = true;
+            }
+            else
+            {
+                boardController.StartImageWait(GetSymbols);
+            }
+            //If the words are loaded then it starts generating the board
+            if (wordsLoaded)
+            {
+                //deactives all current active lettercubes
+                foreach (LetterCube lC in activeLetterCubes)
+                {
+                    lC.Deactivate();
+                }
+                int count = Random.Range(minWrongLetters, maxWrongLetters);
+                activeLetterCubes.Clear();
+                //finds new letterboxes to be activated and assigns them a random incorrect letter.
+                for (int i = 0; i < count; i++)
+                {
+                    char letter = LetterManager.GetRandomLetters(1)[0];
+                    while (word.Contains(char.ToLower(letter)))
+                    {
+                        letter = LetterManager.GetRandomLetters(1)[0];
+                    }
+                    LetterCube potentialCube = letterCubes[Random.Range(0, letterCubes.Count)];
 
                     //Check to ensure letters dont spawn below the player and that it is not an allready activated lettercube
                     while (activeLetterCubes.Contains(potentialCube))
@@ -161,51 +164,51 @@ namespace Scenes.Minigames.SymbolEater.Scripts.Gamemodes
 
         }
 
-    /// <summary>
-    /// Replaces an active lettercube with another one
-    /// </summary>
-    /// <param name="letter">The letter which should be replaced</param>
-    public void ReplaceSymbol(LetterCube letter)
-    {
-        //Updates the display of letters which the player has already found
-        if(foundLetters.Count > 0 && letter.GetLetter() == foundLetters.Peek().ToString())
+        /// <summary>
+        /// Replaces an active lettercube with another one
+        /// </summary>
+        /// <param name="letter">The letter which should be replaced</param>
+        public void ReplaceSymbol(LetterCube letter)
         {
-            string foundWordPart = "";
-            int j = word.IndexOf(foundLetters.Dequeue());
-            for(int i = 0; i < j + 1; i++)
+            //Updates the display of letters which the player has already found
+            if (foundLetters.Count > 0 && letter.GetLetter() == foundLetters.Peek().ToString())
             {
-                if(word.Length > i)
+                string foundWordPart = "";
+                int j = word.IndexOf(foundLetters.Dequeue());
+                for (int i = 0; i < j + 1; i++)
                 {
-                    foundWordPart += word[i];
+                    if (word.Length > i)
+                    {
+                        foundWordPart += word[i];
+                    }
+                }
+                boardController.SetAnswerText(foundWordPart);
+            }
+            string oldLetter = letter.GetLetter();
+            letter.Deactivate();
+            activeLetterCubes.Remove(letter);
+
+            LetterCube newLetter;
+            //finds a new random letterbox which is not active and is not the one which should be replaced
+            while (true)
+            {
+                newLetter = letterCubes[Random.Range(0, letterCubes.Count)];
+                if (newLetter != letter && !activeLetterCubes.Contains(newLetter))
+                {
+                    break;
                 }
             }
-            boardController.SetAnswerText(foundWordPart);
-        }
-        string oldLetter = letter.GetLetter();
-        letter.Deactivate();
-        activeLetterCubes.Remove(letter);
-        
-        LetterCube newLetter;
-        //finds a new random letterbox which is not active and is not the one which should be replaced
-        while(true)
-        {
-            newLetter = letterCubes[Random.Range(0, letterCubes.Count)];
-            if(newLetter != letter && !activeLetterCubes.Contains(newLetter))
+            activeLetterCubes.Add(newLetter);
+            if (currentIndex < word.Length)
             {
-                break;
-            }
-        }
-        activeLetterCubes.Add(newLetter);
-        if(currentIndex < word.Length)
-        {
-            //currentLetter = word[currentIndex];
-            char newLettercubeValue = LetterManager.GetRandomLetter();
-            if(word.Contains(oldLetter))
-            {
-                newLettercubeValue = oldLetter[0];
-            }
+                //currentLetter = word[currentIndex];
+                char newLettercubeValue = LetterManager.GetRandomLetter();
+                if (word.Contains(oldLetter))
+                {
+                    newLettercubeValue = oldLetter[0];
+                }
 
-            newLetter.Activate(newLettercubeValue.ToString());
+                newLetter.Activate(newLettercubeValue.ToString());
 
             }
             else
@@ -255,29 +258,30 @@ namespace Scenes.Minigames.SymbolEater.Scripts.Gamemodes
             maxWrongLetters = max;
         }
 
-    /// <summary>
-    /// If the imageManager hasnt finished loading the images at startup, this method will setup the answer image after it has finished.
-    /// </summary>
-    public void OnImageLoad()
-    {
-        if(words.Count > 0)
+        /// <summary>
+        /// If the imageManager hasnt finished loading the images at startup, this method will setup the answer image after it has finished.
+        /// </summary>
+        public void OnImageLoad()
         {
-            Texture2D texture = ImageManager.GetImageFromWord(word);
-            sprites.Add(word, Sprite.Create(texture, new Rect(0.0f, 0.0f, texture.width, texture.height), new Vector2(0.5f, 0.5f), 100.0f));
-            boardController.SetImage(sprites[word]);
+            if (words.Count > 0)
+            {
+                Texture2D texture = ImageManager.GetImageFromWord(word);
+                sprites.Add(word, Sprite.Create(texture, new Rect(0.0f, 0.0f, texture.width, texture.height), new Vector2(0.5f, 0.5f), 100.0f));
+                boardController.SetImage(sprites[word]);
+            }
+            else
+            {
+                GetSymbols();
+            }
         }
-        else 
-        {
-            GetSymbols();
-        }
-    }
 
-    /// <summary>
-    /// Temporarily unused until relevant game rules have been implemented
-    /// </summary>
-    /// <param name="gameRules">game rules to be used by the game mode</param>
-    public void SetGameRules(IGameRules gameRules)
-    {
-        
+        /// <summary>
+        /// Temporarily unused until relevant game rules have been implemented
+        /// </summary>
+        /// <param name="gameRules">game rules to be used by the game mode</param>
+        public void SetGameRules(IGameRules gameRules)
+        {
+
+        }
     }
 }
