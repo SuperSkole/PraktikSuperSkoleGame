54aca34320f7f3cf3f79a80a5ffc88d82d5a8730
Wingmoon5150 <54482@student.eadania.dk>
2024-08-23 12:25:46 +0200

    Added generic gamemode and minigamesetup interfaces

---
+++ Assets/CORE/Scripts/IGenericGameMode.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IGenericGameMode
+{
+    //all game modes should use this interface in order to be called as a generic gamemode
+}
---
+++ Assets/CORE/Scripts/IMinigameSetup.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CORE.Scripts
+{
+    public interface IMinigameSetup
+    {
+        /// <summary>
+        /// Use this for whatever function sets up the main game mode and rules of your minigame, usually replaces a start function
+        /// </summary>
+        /// <param name="gameMode">takes a gamemode that your manager should use</param>
+        /// <param name="gameRules">takes a game rule set that your manager should use</param>
+        public void SetupGame(IGenericGameMode gameMode, IGameRules gameRules);
+    }
+}
