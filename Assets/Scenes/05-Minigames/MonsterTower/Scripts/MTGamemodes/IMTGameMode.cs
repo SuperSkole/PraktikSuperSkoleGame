68a13134f9e4b3dcedbb7eed21dc4fe86902da6c
Luna <112692450+Wingmoon5150@users.noreply.github.com>
2024-08-23 12:00:02 +0200

    Luna UI gamemodes (#84)
    * i don't even know, github did this
    
    * added new minigame specific selection scenes
    
    * created the code base for Monster Tower Game Modes
    
    * Made gamemode capable of generating sentences
    
    * Added Namespaces
    
    * Fixed merge issues
    
    * fixed comments

---
+++ Assets/Scenes/05-Minigames/MonsterTower/Scripts/MTGamemodes/IMTGameMode.cs
@@ -0,0 +1,38 @@
+using Scenes.Minigames.MonsterTower;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Scenes.Minigames.MonsterTower.Scrips;
+
+namespace Scenes.Minigames.MonsterTower.Scrips.MTGameModes
+{
+    public interface IMTGameMode
+    {
+        /// <summary>
+        /// Will be called by the TowerManager to create a brick with the correct answer
+        /// </summary>
+        /// <param name="str">the correct answer will have to take a string to find the correct image using the ImageManager, or have a string for text</param>
+        /// <param name="manager">a reference back to the tower manager so it can modify the tower manager</param>
+        public void SetCorrectAnswer(string str, TowerManager manager);
+
+        /// <summary>
+        /// Will be called by the TowerManager to create a brick with an (usually random) incorrect answer
+        /// </summary>
+        /// <param name="manager">a reference back to the tower manager so it can modify the tower manager</param>
+        public void SetWrongAnswer(TowerManager manager);
+
+        /// <summary>
+        /// Set's the answer key, which will tell the player which brick is correct. Uses the opposite medium of SetCorrectAnswer
+        /// </summary>
+        /// <param name="str">The answer key will have to take a string to find the correct image using the ImageManager, or have a string for text</param>
+        /// <param name="manager">a reference back to the tower manager so it can modify the tower manager</param>
+        public void GetDisplayAnswer(string str, TowerManager manager);
+
+        /// <summary>
+        /// create a series of answers
+        /// </summary>
+        /// <param name="count">number of answers to create</param>
+        /// <returns>Returns a set of answers strings to be used by the towerManager</returns>
+        public string[] GenerateAnswers(int count);
+    }
+}
