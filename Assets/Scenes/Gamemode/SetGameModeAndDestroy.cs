fe4080b5a9fcaee79131fb5a68663e4ff001815b
Wingmoon5150 <54482@student.eadania.dk>
2024-08-26 11:00:08 +0200

    added namespace

--- Assets/Scenes/Gamemode/SetGameModeAndDestroy.cs
+++ Assets/Scenes/Gamemode/SetGameModeAndDestroy.cs
@@ -10,39 +10,40 @@ using Scenes.Minigames.SymbolEater.Scripts;
 using CORE.Scripts;
 using CORE.Scripts.GameRules;
 
-
-
-public class SetGameModeAndDestroy : MonoBehaviour
+namespace Scenes.GameMode
 {
-
-    private IGenericGameMode gamemode;
-    private IGameRules gameRule;
-    //tells the scene manager to call the OnSceneLoaded function whenever a scene is loaded
-    private void Start()
+    public class SetGameModeAndDestroy : MonoBehaviour
     {
-        SceneManager.sceneLoaded += OnSceneLoaded;
-    }
+
+        private IGenericGameMode gamemode;
+        private IGameRules gameRule;
+        //tells the scene manager to call the OnSceneLoaded function whenever a scene is loaded
+        private void Start()
+        {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
 
 
-    //Finds an object with the tag "setup" in the scene, then runs the expected MiniGameSetup interface's method, then kills itself to avoid memory leak
-    void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
-    {
-        GameObject parentToTarget = GameObject.FindGameObjectWithTag("Setup");
-        IMinigameSetup target = parentToTarget.GetComponent<IMinigameSetup>();
-        target.SetupGame(gamemode, gameRule);
-        
+        //Finds an object with the tag "setup" in the scene, then runs the expected MiniGameSetup interface's method, then kills itself to avoid memory leak
+        void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
+        {
+            GameObject parentToTarget = GameObject.FindGameObjectWithTag("Setup");
+            IMinigameSetup target = parentToTarget.GetComponent<IMinigameSetup>();
+            target.SetupGame(gamemode, gameRule);
 
-        SceneManager.sceneLoaded -= OnSceneLoaded;
 
-        Destroy(gameObject);
-    }
-    //sets a value so the OnSceneChange script can correctly determine which objects to look for and what modes to set
-    public void Setgamemode(IGenericGameMode gamemodeID)
-    {
-        gamemode = gamemodeID;
-    }
-    public void SetGameRules(IGameRules gameRuleID)
-    {
-        gameRule = gameRuleID;
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+
+            Destroy(gameObject);
+        }
+        //sets a value so the OnSceneChange script can correctly determine which objects to look for and what modes to set
+        public void Setgamemode(IGenericGameMode gamemodeID)
+        {
+            gamemode = gamemodeID;
+        }
+        public void SetGameRules(IGameRules gameRuleID)
+        {
+            gameRule = gameRuleID;
+        }
     }
 }
