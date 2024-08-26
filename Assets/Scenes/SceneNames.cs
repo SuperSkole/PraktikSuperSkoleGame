9f858a84f0fe3b7e2d30c2e448e8f6732b29e7bf
MTBonde <thorbonde@gmail.com>
2024-08-26 15:39:47 +0200

    initial code for gamemode

--- Assets/Scenes/SceneNames.cs
+++ Assets/Scenes/SceneNames.cs
@@ -2,11 +2,21 @@ namespace Scenes
 {
     public static class SceneNames
     {
+        public const string Boot = "Bootstrapper";
         public const string Login = "00-LoginScene";
         public const string Start = "01-StartScene";
-        public const string Player = "PlayerScene";
         public const string House = "02-PlayerHouse";
-        public const string Main = "03-MainWorld";
-        public const string Factory = "WordFactory";
+        public const string Player = "03-PlayerScene";
+        public const string Main = "05-MainWorld";
+
+        // 50-minigames
+        public const string Gamemode = "50-GameModeSelector";
+        public const string Letter = "51-LetterGardenScene";
+        public const string Eater = "52-SymbolEater";
+        public const string Factory = "53-WordFactory";
+        public const string Racer = "55-RacingGame";
+        public const string Bank = "56-BankGame";
+        public const string Breakin = "57-BreakInGame";
+        public const string Tower = "59-MonsterTower";
     }
 }
