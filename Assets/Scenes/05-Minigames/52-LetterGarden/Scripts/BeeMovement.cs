8ffac415e87a38689163f49d5172a948cb2fc46e
harbocasper <sofieharbojensen@gmail.com>
2024-08-27 15:44:04 +0200

    started working on lettergarden

--- Assets/Scenes/05-Minigames/52-LetterGarden/Scripts/BeeMovement.cs
+++ Assets/Scenes/05-Minigames/52-LetterGarden/Scripts/BeeMovement.cs
@@ -10,8 +10,6 @@ namespace Scenes.Minigames.LetterGarden.Scrips
         [SerializeField] private GameObject SplineParent;
 
         public SplineContainer letterSpline;
-        private readonly List<SplineContainer> letterList = new();
-        private readonly List<SplineContainer> lettersToDraw = new();
 
         private Vector3 currentPos;
         private Vector3 direction;
@@ -19,66 +17,17 @@ namespace Scenes.Minigames.LetterGarden.Scrips
         private float spineLeangth;
         public int splineIndex = 0;
 
-        private readonly int difficultTest = 1;
-        private readonly int difficultyEasy = 3;
-        private readonly int difficultyMedium = 5;
-        private readonly int difficultyHard = 7;
-        private readonly int difficultyAll;
-        private int difficultyCurrent = 3;
-        private int completedLetters = 0;
-
-        /// <summary>
-        /// Runs at start and dynamically fetches all splines used to draw letters/symbols, then selects one at random.
-        /// </summary>
-        private void Start()
-        {
-            SetDifficulty(difficultyEasy); //TODO: Placeholder until difficulty selection is created
-            //SetDifficulty(difficultTest);
-            SetLettersToDraw();
-            NextLetter();
-        }
-
-        private void SetDifficulty(int difficulty)
-        {
-            difficultyCurrent = difficulty;
-        }
-
-        /// <summary>
-        /// Finds all possible letters and assigns a number to be drawn depending on difficulty level.
-        /// </summary>
-        private void SetLettersToDraw()
-        {
-            foreach (Transform spline in SplineParent.GetComponentInChildren<Transform>())
-            {
-                letterList.Add(spline.gameObject.GetComponent<SplineContainer>());
-            }
-            if (difficultyCurrent > letterList.Count)
-            {
-                Debug.LogError("The difficulty level for LetterGarden is attempting to load more letters than is available.");
-            }
-            for (completedLetters = 0; completedLetters < difficultyCurrent; completedLetters++)
-            {
-                SplineContainer currentLetter = letterList[Random.Range(0, letterList.Count)];
-                lettersToDraw.Add(currentLetter);
-                letterList.Remove(currentLetter);
-            }
-        }
 
         /// <summary>
         /// Called to switch to the next letter, once the previous one has been completed.
         /// </summary>
-        private void NextLetter()
+        public void NextLetter(SplineContainer currentLetter)
         {
-            EarnReward();
             if (lettersToDraw.Count > 0)
             {
-                letterSpline = lettersToDraw[0];
+                letterSpline = currentLetter;
                 lettersToDraw.Remove(letterSpline);
                 spineLeangth = letterSpline.CalculateLength(splineIndex);
-            }
-            else
-            {
-                VictoryCondition();
             }
         }
 
@@ -87,7 +36,6 @@ namespace Scenes.Minigames.LetterGarden.Scrips
             if (letterSpline != null)
             {
                 MoveOnSpline();
-
                 CheckDistance();
             }
         }
--- Assets/Scenes/05-Minigames/52-LetterGarden/Scripts/SymbolManager.cs
+++ Assets/Scenes/05-Minigames/52-LetterGarden/Scripts/SymbolManager.cs
@@ -78,5 +78,7 @@ namespace Minigames.LetterGarden.Scripts
                 }
             }
         }
+
+
     }
 }
