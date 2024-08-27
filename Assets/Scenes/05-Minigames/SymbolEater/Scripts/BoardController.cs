9ed3f38829589d512f00b72a3c2ecefafddde56e
ewrefads <gdonovan@live.dk>
2024-08-27 11:17:23 +0200

    boardcontroller cleanup

--- Assets/Scenes/05-Minigames/SymbolEater/Scripts/BoardController.cs
+++ Assets/Scenes/05-Minigames/SymbolEater/Scripts/BoardController.cs
@@ -1,14 +1,11 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using CORE.Scripts;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using Scenes.Minigames.SymbolEater.Scripts.Gamemodes;
 using CORE.Scripts.GameRules;
-using CORE.Scripts;
 using Scenes.PlayerScene.Scripts;
 
 namespace Scenes.Minigames.SymbolEater.Scripts
@@ -29,6 +26,8 @@ namespace Scenes.Minigames.SymbolEater.Scripts
         [SerializeField] private GameObject answerImageObject;
 
         [SerializeField] private GameObject gameOverObject;
+
+
 
         private TextMeshProUGUI answerText;
 
@@ -60,6 +59,10 @@ namespace Scenes.Minigames.SymbolEater.Scripts
         /// <param name="targetMode">The game mode which should be used</param>
         public void SetupGame(IGenericGameMode targetMode, IGameRules targetRules)
         {
+            //Backup code in case the dataloader hasnt been created
+            if(!DataLoader.IsDataLoaded){
+                gameObject.AddComponent<DataLoader>();
+            }
             //Sets various fieldvariables and their field variables
             gameMode = (ISEGameMode)targetMode;
             player = playerObject.GetComponent<SymbolEaterPlayer>();
@@ -93,7 +96,7 @@ namespace Scenes.Minigames.SymbolEater.Scripts
         /// </summary>
         private void Start()
         {
-            //SetupGame(new FindNumber(), new FindNumberSeries());
+            SetupGame(new SpellWordFromImage(), new SpellWord());
         }
 
         #endregion
