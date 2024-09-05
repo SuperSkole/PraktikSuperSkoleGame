f602baa88972901c84f3a1830006095050f835e2
Wingmoon5150 <54482@student.eadania.dk>
2024-09-05 08:37:15 +0200

    added balloon arcade game

---
+++ Assets/Scenes/70-Arcade/Scripts/BalloonController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BalloonController : MonoBehaviour
+{
+    public bool isCorrect;
+    public string letter;
+    [SerializeField]private int targetX;
+    [SerializeField]private float speed;
+
+    void Start()
+    {
+        targetX = Random.Range(0, 1800);
+        speed = Random.Range(5f, 7f);
+        //makes the button only work if the image is clicked
+        this.GetComponent<Image>().alphaHitTestMinimumThreshold = 0.1f;
+
+        this.GetComponentInChildren<TMP_Text>().text = letter;
+        LeanTween.move(gameObject, new Vector3(targetX, 1450, 0), speed).setOnComplete(DestroyMe);
+    }
+
+    void DestroyMe()
+    {
+        if(isCorrect)
+        {
+            BalloonSpawner mother = GetComponentInParent<BalloonSpawner>();
+            mother.lives--;
+            if (mother.lives <= 0)
+            {
+                SwitchScenes.SwitchToArcadeScene();
+            }
+        }
+        if (!isCorrect)
+        {
+
+        }
+        
+        Destroy(gameObject);
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Clicked()
+    {
+        if(isCorrect)
+        {
+            BalloonSpawner mother = GetComponentInParent<BalloonSpawner>();
+            mother.points++;
+        }
+        if(!isCorrect)
+        {
+
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scenes/70-Arcade/Scripts/BalloonSpawner.cs
@@ -0,0 +1,63 @@
+using CORE.Scripts.GameRules;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class BalloonSpawner : MonoBehaviour
+{
+    public GameObject balloonPrefab;
+    [SerializeField] private float spawntimer = 1f;
+    private Vector3 spawnpoint;
+    string correctLetter;
+    string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ∆ÿ≈";
+    public int lives;
+    public int points;
+    TMP_Text text;
+    void Start()
+    {
+        spawnpoint.z = 0;
+        spawnpoint.y = 0;
+        correctLetter += letters[Random.Range(0, letters.Length)];
+        StartCoroutine(BalloonWave());
+        text = GetComponentInChildren<TMP_Text>();
+    }
+
+    private void Update()
+    {
+        text.text = $"Click all {correctLetter} \n You have clicked {points} \n you have {lives}/3 lives";
+    }
+
+    private void SpawnBalloon()
+    {
+        spawnpoint.x = Random.Range(0, 1800);
+        GameObject a = Instantiate(balloonPrefab, spawnpoint, this.transform.rotation, this.transform);
+        BalloonController balloonSpawned = a.GetComponent<BalloonController>();
+        if(Random.Range(0, 3)==0)
+        {
+            balloonSpawned.letter = correctLetter;
+            balloonSpawned.isCorrect = true;
+        }
+        else
+        {
+            balloonSpawned.letter = "" + letters[Random.Range(0, letters.Length)];
+            if(balloonSpawned.letter == correctLetter)
+            {
+                balloonSpawned.isCorrect = true;
+            }
+        }
+    }
+
+    IEnumerator BalloonWave()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(spawntimer);
+            SpawnBalloon();
+            spawntimer = Random.Range(0.5f, 2f);            
+        }
+    }
+
+    
+}
