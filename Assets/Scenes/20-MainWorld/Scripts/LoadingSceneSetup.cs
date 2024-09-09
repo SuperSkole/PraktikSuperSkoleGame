d10909f5ccce6c968d2fc8db208d9a2575db567b
ewrefads <gdonovan@live.dk>
2024-09-09 08:48:29 +0200

    Added comments

--- Assets/Scenes/20-MainWorld/Scripts/LoadingSceneSetup.cs
+++ Assets/Scenes/20-MainWorld/Scripts/LoadingSceneSetup.cs
@@ -9,12 +9,20 @@ public class LoadingSceneSetup : MonoBehaviour
 {
     [SerializeField]private int sceneID;
 
+    /// <summary>
+    /// Adds OnSceneLoaded to be run then a scene is loaded and begins loading the gamemode selector.
+    /// </summary>
     public void Load()
     {
         SceneManager.sceneLoaded += OnSceneLoaded;
         SwitchScenes.SwitchToMinigameLoadingScene();
     }
 
+    /// <summary>
+    /// Finds the SetGameModeAndDestroy script and sets its sceneid. Removes itself as a method to be run on sceneload afterwards.
+    /// </summary>
+    /// <param name="scene">not used</param>
+    /// <param name="loadSceneMode">not used</param>
     void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
     {
         GameObject gamemode = GameObject.FindGameObjectWithTag("GamemodeSelect");
