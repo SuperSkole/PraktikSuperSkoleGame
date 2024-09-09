e0dc169811bb2fbd3d82f84b3df1767658d53057
ewrefads <clara.donovan@outlook.com>
2024-09-09 08:49:39 +0200

    Clara branch 2.1 (#200)
    * Symbol eater bugfixing
    
    * Added gamemode to recognize name of letter to symbol eater
    
    * Begun work on lettergarden gamemode rework
    
    * misc. scene changes
    
    * Added level based gamemodes to lettergarden and related game rules
    
    * Added  unsaved comment
    
    * Added another unsaved comment
    
    * Added universal level selector for minigames
    
    * Merge fixes and added some comments
    
    * Added comments

--- Assets/Scenes/50-Minigames/52-LetterGarden/Scripts/SymbolManager.cs
+++ Assets/Scenes/50-Minigames/52-LetterGarden/Scripts/SymbolManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using CORE.Scripts;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Splines;
@@ -17,16 +18,18 @@ namespace Scenes.Minigames.LetterGarden.Scripts
         /// a gameobject gets added to this list if it has a name with the format Capital<Letter>
         /// </summary>
         public static Dictionary<char, GameObject> capitalLettersObjects;
+
         /// <summary>
-        /// a gameobject gets added to this list if it has a name with the format Capital<Letter>
+        /// a spline gets added to this list if it has a name with the format Capital<Letter>
         /// </summary>
         public static Dictionary<char, SplineContainer> capitalLetters;
+
         /// <summary>
         /// a gameobject gets added to this list if it has a name with the format Lowercase<Letter>
         /// </summary>
         public static Dictionary<char, GameObject> lowercaseLettersObjects;
         /// <summary>
-        /// a gameobject gets added to this list if it has a name with the format Lowercase<Letter>
+        /// a spline gets added to this list if it has a name with the format Lowercase<Letter>
         /// </summary>
         public static Dictionary<char, SplineContainer> lowercaseLetters;
         /// <summary>
@@ -34,17 +37,29 @@ namespace Scenes.Minigames.LetterGarden.Scripts
         /// </summary>
         public static Dictionary<int, GameObject> numbersObjects;
         /// <summary>
-        /// a gameobject gets added to this list if it can be converted to an int
+        /// a spline gets added to this list if it can be converted to an int
         /// </summary>
         public static Dictionary<int, SplineContainer> numbers;
 
+        /// <summary>
+        /// a gameobject gets added to this list if it has a name used as a key in symbolnames
+        /// </summary>
+        public static Dictionary<char, GameObject> symbolObjects;
+
+        /// <summary>
+        /// a spline gets added to this list if it has a name used as a key in symbolnames
+        /// </summary>
+        public static Dictionary<char, SplineContainer> symbols;
+
         [SerializeField] private List<GameObject>symbolPrefabs;
+        
+        private Dictionary<string, char> symbolNames = new Dictionary<string, char>();
         /// <summary>
         /// Takes the gameobjects from the symbolprefabs list and sorts them into the various dictionaries
         /// </summary>
         public void StartLoad()
         {
-            
+            AddSymbolNames();
             capitalLettersObjects = new Dictionary<char, GameObject>();
             lowercaseLettersObjects = new Dictionary<char, GameObject>();
             numbersObjects = new Dictionary<int, GameObject>();
@@ -88,6 +103,11 @@ namespace Scenes.Minigames.LetterGarden.Scripts
                     numbersObjects.Add(numName, gameObject);
                     numbers.Add(numName, gameObject.GetComponent<SplineContainer>());
                 }
+                else if(symbolNames.ContainsKey(gameObject.name))
+                {
+                    symbolObjects.Add(symbolNames[gameObject.name], gameObject);
+                    symbols.Add(symbolNames[gameObject.name], gameObject.GetComponent<SplineContainer>());
+                }
                 //Error in case a gameobject gets added to the list on accident or the name is wrongly formated
                 else
                 {
@@ -95,6 +115,12 @@ namespace Scenes.Minigames.LetterGarden.Scripts
                 }
             }
         }
+
+        private void AddSymbolNames()
+        {
+            symbolNames.Add("Ampersand", '&');
+        }
+
 
         /// <summary>
         /// Translates the letter replacement from files t the equivelent danish letter
--- Assets/Scenes/50-Minigames/54-SymbolEater/Scripts/Gamemodes/GameModeHelper.cs
+++ Assets/Scenes/50-Minigames/54-SymbolEater/Scripts/Gamemodes/GameModeHelper.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using CORE.Scripts.Game_Rules;
 using UnityEngine;
 
 namespace Scenes._50_Minigames._54_SymbolEater.Scripts.Gamemodes 
@@ -20,7 +21,7 @@ namespace Scenes._50_Minigames._54_SymbolEater.Scripts.Gamemodes
         /// <param name="activeLetterCubes">all currently active lettercubes</param>
         /// <param name="getLetterCubeValue">method which activates the lettercube</param>
         /// <param name="correct">whether the value on the cube should be the correct one</param>
-        public static void ActivateLetterCubes(int amount, List<LetterCube> letterCubes, List<LetterCube>activeLetterCubes, GetLetterCubeValue getLetterCubeValue, bool correct)
+        public static void ActivateLetterCubes(int amount, List<LetterCube> letterCubes, List<LetterCube>activeLetterCubes, GetLetterCubeValue getLetterCubeValue, bool correct, IGameRules gameRules)
         {
             //Activates the given amount of lettercubes
             for(int i = 0; i < amount; i++)
@@ -33,6 +34,26 @@ namespace Scenes._50_Minigames._54_SymbolEater.Scripts.Gamemodes
                 }
                 activeLetterCubes.Add(potientialCube);
                 getLetterCubeValue(potientialCube, correct);
+            }
+            int foundLetterCubes = 0;
+            foreach(LetterCube letterCube in activeLetterCubes)
+            {
+                if(correct && gameRules.IsCorrectSymbol(letterCube.GetLetter()))
+                {
+                    foundLetterCubes++;
+                }
+                else if(!correct && !gameRules.IsCorrectSymbol(letterCube.GetLetter()))
+                {
+                    foundLetterCubes++;
+                }
+                if(foundLetterCubes == amount)
+                {
+                    break;
+                }
+            }
+            if(foundLetterCubes < amount)
+            {
+                ActivateLetterCubes(amount - foundLetterCubes, letterCubes, activeLetterCubes, getLetterCubeValue, correct, gameRules);
             }
         }
 
