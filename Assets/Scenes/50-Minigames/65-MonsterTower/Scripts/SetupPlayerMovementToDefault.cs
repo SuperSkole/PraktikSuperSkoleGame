4b4536c085f9dec861f8587bf9196559a0abf738
Henr9423 <54700@student.eadania.dk>
2024-09-12 13:47:29 +0200

    Playerdata in debug UI

---
+++ Assets/Scenes/50-Minigames/65-MonsterTower/Scripts/SetupPlayerMovementToDefault.cs
@@ -0,0 +1,13 @@
+using Scenes._10_PlayerScene.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetupPlayerMovementToDefault : MonoBehaviour
+{
+    public void SetUpDefaultMovement()
+    {
+        Destroy(PlayerManager.Instance.SpawnedPlayer.GetComponent<PlayerMovement_MT>());
+        Destroy(PlayerManager.Instance.SpawnedPlayer.GetComponent<AutoMovePlayer>());
+    }
+}
