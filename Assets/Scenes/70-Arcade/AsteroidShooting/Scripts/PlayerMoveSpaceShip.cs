814948408aa11898be887d1a86ebac78d7bf621b
Henr9423 <54700@student.eadania.dk>
2024-09-16 13:21:41 +0200

    Asteroid

--- Assets/Scenes/70-Arcade/AsteroidShooting/Scripts/PlayerMoveSpaceShip.cs
+++ Assets/Scenes/70-Arcade/AsteroidShooting/Scripts/PlayerMoveSpaceShip.cs
@@ -5,29 +5,53 @@ using UnityEngine;
 
 public class PlayerMoveSpaceShip : MonoBehaviour
 {
-    [SerializeField] int speed; 
+    [SerializeField] int speed;
+    [SerializeField] float shipAcceleration;
+    [SerializeField] Rigidbody rigidBody;
+
+    Vector3 forceOnPlayer;
+    Vector3 rotation=Vector3.zero;
+    private bool isAccelerating;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        shipAcceleration = 100;
+    }
+
+    private void Update()
+    {
+        isAccelerating = Input.GetAxis("Vertical") > 0f;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
 
-        if(Input.GetAxis("Vertical") > 0f)
+        if (isAccelerating)
         {
-            gameObject.transform.Translate(new Vector3(0, speed, 0));
+            rigidBody.AddForce(transform.up*shipAcceleration);
+            rigidBody.velocity = Vector3.ClampMagnitude(rigidBody.velocity, 100);
+            
+        }
+        
+
+        if(Input.GetAxis("Horizontal")>0f)
+        { 
+           
+            transform.Rotate(new Vector3(0,0,-2f));
+        }
+        else if (Input.GetAxis("Horizontal") < 0f)
+        {
+            transform.Rotate(new Vector3(0,0,2f));
         }
 
-        if (Input.GetAxis("Vertical") < 0f)
-        {
-            gameObject.transform.Translate(new Vector3(0, -speed, 0));
-        }
+
 
 
 
     }
+
+
+    
 }
