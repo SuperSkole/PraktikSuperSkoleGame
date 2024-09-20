01dbf3dea17e53502d1677f0b05b4510277934ab
Casper3667 <CasperMC@hotmail.dk>
2024-09-20 13:55:07 +0200

    Friday Work

--- Assets/Scenes/Multiplayer/HelloWorldPlayer.cs
+++ Assets/Scenes/Multiplayer/HelloWorldPlayer.cs
@@ -2,8 +2,11 @@ using LoadSave;
 using Scenes._10_PlayerScene.Scripts;
 using Spine.Unity;
 using System.Collections.Generic;
+using Unity.Collections;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.ProBuilder.Shapes;
+using static UnityEngine.CullingGroup;
 
 namespace HelloWorld
 {
@@ -18,34 +21,75 @@ namespace HelloWorld
             {"white", 4 },
             {"pink", 5 }
         };
-        ColorChanging colorChange;
-        string color;
 
+        List<Color> colorList = new() { Color.red, Color.blue, Color.cyan, Color.green, Color.grey, Color.yellow, Color.magenta };
+
+        //ColorChanging colorChange;
+        Color color = Color.red;
+        SpriteRenderer sprite;
 
         public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
-        //public NetworkVariable<string> randombullshit = new NetworkVariable<string>();
-        public NetworkVariable<int> colorPick = new NetworkVariable<int>(); // This somehow break stuff !!DO NOT USE!!
+        public NetworkVariable<FixedString32Bytes> colorPick = new NetworkVariable<FixedString32Bytes>();
+        public NetworkVariable<Color> colorUsed = new NetworkVariable<Color>();
         public override void OnNetworkSpawn()
         {
+            //colorPick.OnValueChanged += OnStateChanged;
+            sprite = GetComponent<SpriteRenderer>();
             if (IsOwner)
             {
                 Move();
                 #region Set up color
-                ISkeletonComponent skeleton = GetComponentInChildren<ISkeletonComponent>();
-                if (skeleton == null)
-                {
-                    Debug.LogError("PlayerManager.SetupPlayer(): " +
-                                   "ISkeleton component not found on spawned player.");
-                    return;
-                }
-                colorChange = GetComponent<ColorChanging>();
-                colorChange.SetSkeleton(skeleton);
-
-                GameObject originPlayer = GameObject.Find("PlayerMonster");
-                color = originPlayer.GetComponent<PlayerData>().MonsterColor;
-                colorChange.ColorChange(color);
+                //ISkeletonComponent skeleton = GetComponentInChildren<ISkeletonComponent>();
+                //if (skeleton == null)
+                //{
+                //    Debug.LogError("PlayerManager.SetupPlayer(): " +
+                //                   "ISkeleton component not found on spawned player.");
+                //    return;
+                //}
+                //colorChange = GetComponent<ColorChanging>();
+                //colorChange.SetSkeleton(skeleton);
+                
+                //GameObject originPlayer = GameObject.Find("PlayerMonster");
+                //color = originPlayer.GetComponent<PlayerData>().MonsterColor;
+                //color = "pink";
+                //colorChange.ColorChange(color);
+                Debug.Log("test 1");
+                //ToggleServerRpc(color);
+                //colorPick.Value = color;
+                //colorChange.ColorChange(colorPick.Value.ToString());
+                Debug.Log("test 2");
+                //GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+                colorUsed.OnValueChanged += OnStateChanged;
+                color = colorList[Random.Range(0, colorList.Count)];
+                colorUsed.Value = color;
+                //ServerUpdateColor(color);
                 #endregion
             }
+        }
+
+        [ServerRpc(RequireOwnership = false)]
+        public void ServerUpdateColorServerRPC(Color newColor)
+        {
+            sprite.color = newColor;
+            //colorChange.ColorChange(newColor);
+            UpdateColorClientRPC(newColor);
+        }
+
+        [ClientRpc]
+        void UpdateColorClientRPC(Color newColor)
+        {
+            //colorChange.ColorChange(newColor);
+            sprite.color = newColor;
+        }
+
+        public void OnStateChanged(Color previous, Color current)
+        {
+            ServerUpdateColorServerRPC(current);
+        }
+
+        public override void OnNetworkDespawn()
+        {
+            //colorPick.OnValueChanged -= OnStateChanged;
         }
 
         public void Move()
@@ -58,7 +102,7 @@ namespace HelloWorld
         {
             var randomPosition = GetRandomPositionOnPlane();
             transform.position = randomPosition;
-            //Position.Value = randomPosition;
+            Position.Value = randomPosition;
         }
 
         static Vector3 GetRandomPositionOnPlane()
@@ -68,9 +112,23 @@ namespace HelloWorld
 
         void Update()
         {
-            //transform.position = Position.Value;
+            transform.position = Position.Value;
         }
-        
-        
+
+        //public void OnStateChanged(FixedString32Bytes previous, FixedString32Bytes current)
+        //{
+        //    color = current.ToString();
+        //    if (color == null)
+        //        color = "green";
+        //    colorChange.ColorChange(color);
+        //}
+
+        //[Rpc(SendTo.Server)]
+        //public void ToggleServerRpc(string input)
+        //{
+        //    colorPick.Value = input;
+        //}
+
+
     }
 }
