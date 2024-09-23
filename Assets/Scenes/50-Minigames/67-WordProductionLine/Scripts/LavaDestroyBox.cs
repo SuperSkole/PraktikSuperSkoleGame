18a7dbf286aa3f300f5b551445d48c202c10ee85
Marc7451 <Marc7451@gmail.com>
2024-09-23 14:57:23 +0200

    Comments

--- Assets/Scenes/50-Minigames/67-WordProductionLine/Scripts/LavaDestroyBox.cs
+++ Assets/Scenes/50-Minigames/67-WordProductionLine/Scripts/LavaDestroyBox.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class LavaDestroyBox : MonoBehaviour
 {
-
+    /// <summary>
+    /// Lava pool that deactivate boxes when the collide with the lava.
+    /// </summary>
+    /// <param name="other"></param>
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.tag == "ProductionCube")
