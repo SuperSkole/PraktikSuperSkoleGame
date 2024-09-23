f06bd09baf724957ca75e1aa1cada357b06d6b32
Marc7451 <Marc7451@gmail.com>
2024-09-23 12:03:23 +0200

    Lever to stop production line gets pullet.

--- Assets/Scenes/50-Minigames/67-WordProductionLine/Scripts/ConveyorBeltLever.cs
+++ Assets/Scenes/50-Minigames/67-WordProductionLine/Scripts/ConveyorBeltLever.cs
@@ -6,15 +6,55 @@ public class ConveyorBeltLever : MonoBehaviour
 {
 
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject stopBeltLever;
+
+    private ProductionLine[] productionLine;
+
+    public float rotationAngle = 45f;
+    private bool isRotated = false;
+
+
+    private void Start()
     {
-        
+        productionLine = FindObjectsOfType<ProductionLine>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseOver()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            foreach (var item in productionLine)
+            {
+                item.conveyerBeltOn = !item.conveyerBeltOn;
+            }
+
+            RotateLever();
+        }
+    }
+
+
+    void RotateLever()
+    {
+        if (stopBeltLever != null)
+        {
+            // Rotate the lever based on its current state
+            if (isRotated)
+            {
+                // Rotate back to the original position (reset)
+                stopBeltLever.transform.rotation = Quaternion.Euler(0, -90, 135);
+            }
+            else
+            {
+                // Rotate to the desired angle on the Z-axis
+                stopBeltLever.transform.rotation = Quaternion.Euler(0, -90, rotationAngle);
+            }
+
+            // Toggle the rotation state
+            isRotated = !isRotated;
+        }
+        else
+        {
+            Debug.LogError("Lever GameObject is not assigned!");
+        }
     }
 }
