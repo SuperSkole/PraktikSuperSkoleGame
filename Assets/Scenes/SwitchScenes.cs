8f7b1171477394c50a85afc3f99ce04d07cf552c
Henr9423 <112692462+Henr9423@users.noreply.github.com>
2024-09-26 10:51:45 +0200

    Bridgeof danger minigame (#295)
    * HotfFixes To asteroid
    
    * Update
    
    * update
    
    * updae
    
    * Update to loseScreen
    
    * Update
    
    * fixed game over
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    ---------
    
    Co-authored-by: Sofiefox <sofieharbojensen@gmail.com>

--- Assets/Scenes/SwitchScenes.cs
+++ Assets/Scenes/SwitchScenes.cs
@@ -10,6 +10,9 @@ namespace Scenes
         public static void SwitchToWordFactoryLoadingScene() => SceneManager.LoadScene(SceneNames.FactoryLoading);
         public static void SwitchToArcadeAsteroidScene() => SceneManager.LoadScene(SceneNames.ArcadeAsteroid);
         public static void SwitchToArcadeAsteroidLoseScene() => SceneManager.LoadScene(SceneNames.ArcadeAsteroidLoseScreen);
+
+        public static void SwitchToPathOfDanger() => SceneManager.LoadScene(SceneNames.PathOfDanger);
+        public static void SwitchToPathOfDangerLoseScene() => SceneManager.LoadScene(SceneNames.PathOfDangerLoseScreen);
         public static void SwitchToWordFactory() => SceneManager.LoadScene(SceneNames.Factory);
         // TODO : Change this when we have a racing scene
         public static void SwitchToRacingScene() => SceneManager.LoadScene(SceneNames.House);    
