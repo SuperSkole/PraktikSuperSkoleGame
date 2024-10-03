773eeaf89323a33661cc86626f7bb18832bfc099
Sofiefox <sofieharbojensen@gmail.com>
2024-10-03 20:31:57 +0200

    todays progress

--- Assets/Options/Scripts/AudioManager.cs
+++ Assets/Options/Scripts/AudioManager.cs
@@ -2,77 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
-using UnityEngine.Rendering;
+
+public enum SoundType
+{
+    SFX,
+    Music,
+    Voice
+}
 
 public class AudioManager : MonoBehaviour
 {
-    [SerializeField] private AudioMixer audioMixer;
-    [SerializeField] private GameObject muteIcon;
-    [SerializeField] private GameObject menu;
-    private bool muted = false;
-    private float savedmasterVolume = 0f;
-
-    /// <summary>
-    /// used to set the master volume
-    /// </summary>
-    /// <param name="volume">a float between 0 and 1 that represents the volume</param>
-    public void SetMasterVolume(float volume)
+    public static AudioManager Instance { get; private set; }
+    private AudioMixer m_Mixer;
+    private GameObject mCamara;
+    private void Awake()
     {
-        savedmasterVolume = volume;
-        if(muted) return;
-        audioMixer.SetFloat("MasterVolume", Mathf.Log10(volume) * 20f);
+        if(Instance == null)
+            Instance = this;
+        if(mCamara == null)
+            mCamara = FindAnyObjectByType<AudioListener>().gameObject;
     }
 
-    /// <summary>
-    /// used to set the SFX volume
-    /// </summary>
-    /// <param name="volume">a float between 0 and 1 that represents the volume</param>
-    public void SetSFXVolume(float volume)
-    {
-        audioMixer.SetFloat("SFXVolume", Mathf.Log10(volume) * 20f);
-    }
 
-    /// <summary>
-    /// used to set the Music volume
-    /// </summary>
-    /// <param name="volume">a float between 0 and 1 that represents the volume</param>
-    public void SetMusicVolume(float volume)
+    public void PlaySound(AudioClip audioClip, SoundType audioType)
     {
-        audioMixer.SetFloat("MusicVolume", Mathf.Log10(volume) * 20f);
-    }
-
-    /// <summary>
-    /// used to set the Voice volume
-    /// </summary>
-    /// <param name="volume">a float between 0 and 1 that represents the volume</param>
-    public void SetVoiceVolume(float volume)
-    {
-        audioMixer.SetFloat("VoiceVolume", Mathf.Log10(volume) * 20f);
-    }
-
-    /// <summary>
-    /// used to mute and unmute the game. toggles between the 2.
-    /// </summary>
-    public void Mute()
-    {
-        muted = !muted;
-        if (muted)
+        AudioSource source = mCamara.AddComponent<AudioSource>();
+        source.clip = audioClip;
+        switch(audioType)
         {
-            audioMixer.SetFloat("MasterVolume", -80);
-            muteIcon.SetActive(true);
-        }
-        else
-        {
-            audioMixer.SetFloat("MasterVolume", Mathf.Log10(savedmasterVolume) * 20f);
-            muteIcon.SetActive(false);
-        }
-    }
+            case SoundType.SFX:
+                source.outputAudioMixerGroup = m_Mixer.outputAudioMixerGroup;
+                break;
+            case SoundType.Music:
 
-    /// <summary>
-    /// used to go back from the audio menu
-    /// </summary>
-    public void Back()
-    {
-        menu.SetActive(false);
+                break;
+            case SoundType.Voice:
+
+                break;
+        }
     }
 }
