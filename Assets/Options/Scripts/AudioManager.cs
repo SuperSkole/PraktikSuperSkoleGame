40680c98bcf847d2158b059f58d242e329d4992a
Sofiefox <sofieharbojensen@gmail.com>
2024-10-04 08:51:29 +0200

    audio manager done

--- Assets/Options/Scripts/AudioManager.cs
+++ Assets/Options/Scripts/AudioManager.cs
@@ -13,17 +13,24 @@ public enum SoundType
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance { get; private set; }
-    private AudioMixer m_Mixer;
     private GameObject mCamara;
+    [SerializeField] private AudioMixerGroup sfx, music, voice;
     private void Awake()
     {
-        if(Instance == null)
+        DontDestroyOnLoad(gameObject);
+        if (Instance == null)
             Instance = this;
+        else
+            Destroy(gameObject);
         if(mCamara == null)
             mCamara = FindAnyObjectByType<AudioListener>().gameObject;
     }
 
-
+    /// <summary>
+    /// Playes a given audio clip at the sceans audio listener.
+    /// </summary>
+    /// <param name="audioClip">the clip you want to play.</param>
+    /// <param name="audioType">what type of audio you are playing.</param>
     public void PlaySound(AudioClip audioClip, SoundType audioType)
     {
         AudioSource source = mCamara.AddComponent<AudioSource>();
@@ -31,14 +38,44 @@ public class AudioManager : MonoBehaviour
         switch(audioType)
         {
             case SoundType.SFX:
-                source.outputAudioMixerGroup = m_Mixer.outputAudioMixerGroup;
+                source.outputAudioMixerGroup = sfx;
                 break;
             case SoundType.Music:
-
+                source.outputAudioMixerGroup = music;
                 break;
             case SoundType.Voice:
-
+                source.outputAudioMixerGroup = voice;
                 break;
         }
+        source.Play();
+        Destroy(source,audioClip.length);
+    }
+
+    /// <summary>
+    /// Playes a given audio clip at the given point.
+    /// </summary>
+    /// <param name="audioClip">the clip you want to play.</param>
+    /// <param name="audioType">what type of audio you are playing.</param>
+    /// <param name="point">the point you want the audio played at.</param>
+    public void PlaySoud(AudioClip audioClip, SoundType audioType,Vector3 point)
+    {
+        GameObject go = Instantiate(new GameObject(), point, Quaternion.identity);
+        DontDestroyOnLoad(go);
+        AudioSource source = go.AddComponent<AudioSource>();
+        source.clip = audioClip;
+        switch (audioType)
+        {
+            case SoundType.SFX:
+                source.outputAudioMixerGroup = sfx;
+                break;
+            case SoundType.Music:
+                source.outputAudioMixerGroup = music;
+                break;
+            case SoundType.Voice:
+                source.outputAudioMixerGroup = voice;
+                break;
+        }
+        source.Play();
+        Destroy(go, audioClip.length);
     }
 }
