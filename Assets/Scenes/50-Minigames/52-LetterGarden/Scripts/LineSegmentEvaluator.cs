e49fa839cc3389caed744ea1f8d4abc9fecc69a1
Sofiefox <sofieharbojensen@gmail.com>
2024-10-07 13:52:14 +0200

    "fixed" lettergarden

--- Assets/Scenes/50-Minigames/52-LetterGarden/Scripts/LineSegmentEvaluator.cs
+++ Assets/Scenes/50-Minigames/52-LetterGarden/Scripts/LineSegmentEvaluator.cs
@@ -12,8 +12,6 @@ namespace Scenes.Minigames.LetterGarden.Scripts
     public static class LineSegmentEvaluator
     {
         [SerializeField] private static float maxDist = 1;
-
-        [SerializeField] private static float totalCorrectDirectionPrecentageCutoff = 0.8f;
         [SerializeField] private static float ispointAtEndOfSplineCutoff = 0.8f;
         [SerializeField] private static float ispointAtStartOfSplineCutoff = 0.2f;
 
@@ -26,13 +24,8 @@ namespace Scenes.Minigames.LetterGarden.Scripts
         public static bool EvaluateSpline(Spline spline, LineRenderer drawing, Vector3 offset)
         {
             float totalDist = 0;
-            float totalCorrectDirectionPercentage = 0;
-            Vector3 previousDrawingPoint = drawing.GetPosition(0) - offset;
             bool isPointAtEndOfSpline = false;
             bool isPointAtStartOfSpline = false;
-
-            SplineUtility.GetNearestPoint(spline, previousDrawingPoint, out float3 previousNearestSplinePoint, out float previousT);
-
 
             for (int i = 0; i < drawing.positionCount; i++)
             {
@@ -44,23 +37,6 @@ namespace Scenes.Minigames.LetterGarden.Scripts
 
                 distToSpline = Mathf.Clamp(distToSpline, 0, 5);
                 totalDist += distToSpline -0.2f;
-
-                // Validate the direction between consecutive points
-                Vector3 splineDirection = ((Vector3)currentNearestSplinePoint - (Vector3)previousNearestSplinePoint).normalized;
-                Vector3 drawingDirection = (currentDrawingPoint - previousDrawingPoint).normalized;
-
-                float dotProduct = Vector3.Dot(splineDirection, drawingDirection);
-                if (dotProduct > 0)
-                {
-                    totalCorrectDirectionPercentage += 1f / (float)drawing.positionCount;
-                }
-                else
-                {
-                    totalCorrectDirectionPercentage -= 1f / (float)drawing.positionCount;
-                }
-
-                previousDrawingPoint = currentDrawingPoint;
-                previousNearestSplinePoint = currentNearestSplinePoint;
 
                 if (currentT >= ispointAtEndOfSplineCutoff)
                 {
@@ -76,11 +52,6 @@ namespace Scenes.Minigames.LetterGarden.Scripts
             if (!isPointAtEndOfSpline || !isPointAtStartOfSpline) 
             {
                 return false;//checks that the end of the drawing is within the last 20% of the spline
-            }
-
-            if(totalCorrectDirectionPercentage >= totalCorrectDirectionPrecentageCutoff)
-            {
-                return false;
             }
 
             if(totalDist >= maxDist)
