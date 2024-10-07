fdd7b80786f028c2047256d058aef1a39f048915
Sofiefox <sofieharbojensen@gmail.com>
2024-10-07 10:37:41 +0200

    Update LineSegmentEvaluator.cs

--- Assets/Scenes/50-Minigames/52-LetterGarden/Scripts/LineSegmentEvaluator.cs
+++ Assets/Scenes/50-Minigames/52-LetterGarden/Scripts/LineSegmentEvaluator.cs
@@ -15,49 +15,70 @@ namespace Scenes.Minigames.LetterGarden.Scripts
 
         [SerializeField] private static float totalCorrectDirectionPrecentageCutoff = 0.8f;
         [SerializeField] private static float ispointAtEndOfSplineCutoff = 0.8f;
+        [SerializeField] private static float ispointAtStartOfSplineCutoff = 0.2f;
+
         /// <summary>
         /// evaluates the given drawing compared to the spline
         /// </summary>
         /// <param name="spline">the spline of a letter</param>
-        /// <param name="dwaing">the drawing the player has made</param>
+        /// <param name="drawing">the drawing the player has made</param>
         /// <returns>true if the drawing is good enough. false if there are any conflict or wrong thing</returns>
-        public static bool EvaluateSpline(Spline spline, LineRenderer dwaing, Vector3 offset)
+        public static bool EvaluateSpline(Spline spline, LineRenderer drawing, Vector3 offset)
         {
             float totalDist = 0;
-            float totalCorrectDirectionPrecentage = 0;
-            float oldT = -0.1f;
-            bool ispointAtEndOfSpline = false;
-            Vector3 temp = dwaing.GetPosition(0);
-            
-            SplineUtility.GetNearestPoint(spline, temp, out float3 nearest, out float _);
-            for (int i = 0; i < dwaing.positionCount; i++)
+            float totalCorrectDirectionPercentage = 0;
+            Vector3 previousDrawingPoint = drawing.GetPosition(0) - offset;
+            bool isPointAtEndOfSpline = false;
+            bool isPointAtStartOfSpline = false;
+
+            SplineUtility.GetNearestPoint(spline, previousDrawingPoint, out float3 previousNearestSplinePoint, out float previousT);
+
+
+            for (int i = 0; i < drawing.positionCount; i++)
             {
-                Vector3 dwaingPoint = dwaing.GetPosition(i);
-                dwaingPoint.x = 0;
-                dwaingPoint -= offset;
-                float distToSpline = SplineUtility.GetNearestPoint(spline,dwaingPoint,out _,out float t);
-                if (oldT > (t + 0.25f))
+                Vector3 currentDrawingPoint = drawing.GetPosition(i);
+                currentDrawingPoint -= offset;
+                currentDrawingPoint.x = 0;
+
+                float distToSpline = SplineUtility.GetNearestPoint(spline, currentDrawingPoint, out float3 currentNearestSplinePoint, out float currentT);
+
+                distToSpline = Mathf.Clamp(distToSpline, 0, 5);
+                totalDist += distToSpline -0.2f;
+
+                // Validate the direction between consecutive points
+                Vector3 splineDirection = ((Vector3)currentNearestSplinePoint - (Vector3)previousNearestSplinePoint).normalized;
+                Vector3 drawingDirection = (currentDrawingPoint - previousDrawingPoint).normalized;
+
+                float dotProduct = Vector3.Dot(splineDirection, drawingDirection);
+                if (dotProduct > 0)
                 {
-                    totalCorrectDirectionPrecentage -= 1f / (float)dwaing.positionCount;
+                    totalCorrectDirectionPercentage += 1f / (float)drawing.positionCount;
                 }
                 else
                 {
-                    totalCorrectDirectionPrecentage += 1f / (float)dwaing.positionCount;
+                    totalCorrectDirectionPercentage -= 1f / (float)drawing.positionCount;
                 }
-                if(t >= ispointAtEndOfSplineCutoff) ispointAtEndOfSpline = true;
-                oldT = t;
-                distToSpline -= 02f;
-                distToSpline = Mathf.Clamp(distToSpline, 0,5);
-                totalDist += distToSpline;
+
+                previousDrawingPoint = currentDrawingPoint;
+                previousNearestSplinePoint = currentNearestSplinePoint;
+
+                if (currentT >= ispointAtEndOfSplineCutoff)
+                {
+                    isPointAtEndOfSpline = true;
+                }
+                if(currentT <= ispointAtStartOfSplineCutoff)
+                {
+                    isPointAtStartOfSpline = true;
+                }
             }
 
 
-            if (!ispointAtEndOfSpline) 
+            if (!isPointAtEndOfSpline || !isPointAtStartOfSpline) 
             {
                 return false;//checks that the end of the drawing is within the last 20% of the spline
             }
 
-            if(totalCorrectDirectionPrecentage <= totalCorrectDirectionPrecentageCutoff)
+            if(totalCorrectDirectionPercentage >= totalCorrectDirectionPrecentageCutoff)
             {
                 return false;
             }
