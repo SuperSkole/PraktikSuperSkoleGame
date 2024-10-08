dd744c9ecec5775b0609d2ff3fe3759c3c29ed38
theAshamedsleeper <thorbjorn114@gmail.com>
2024-10-08 15:27:46 +0200

    small stuff

--- Assets/UI/Scripts/FindPlayerForButton.cs
+++ Assets/UI/Scripts/FindPlayerForButton.cs
@@ -18,14 +18,14 @@ public class FindPlayerForButton : MonoBehaviour
     }
     public void SpawnCarCloseToPlayer()
     {
-        if (timer > 0.5f)
+        if (timer > 1f)
         {
 
             // Define the spawn position near the player
             Vector3 spawnPosition = PlayerManager.Instance.SpawnedPlayer.transform.position + new Vector3(5, 0, 0);
 
             // Define the size of the box to check for obstacles
-            Vector3 boxSize = new Vector3(2, 1, 4); // Width, height, length of the car
+            Vector3 boxSize = new Vector3(2, 2, 4); // Width, height, length of the car
 
             // Check if the area is clear
             if (!Physics.CheckBox(spawnPosition, boxSize / 2, Quaternion.identity))
@@ -65,32 +65,10 @@ public class FindPlayerForButton : MonoBehaviour
     private void MoveCarToLocation(Vector3 spawnPosition)
     {
         timer = 0;
-
-        //carRigidbody.interpolation = RigidbodyInterpolation.None;
-        //carRigidbody.useGravity = false;
-
-        //// Stop the car completely before moving
-        //carRigidbody.velocity = Vector3.zero;
-        //carRigidbody.angularVelocity = Vector3.zero;
-
-        ////Incase there is something wrong with the cars collision that cases the problem with teleporting the car
-        //var colliderGO = carGO.transform.GetChild(1).GetChild(0).GetComponent<MeshCollider>() ;
-        //colliderGO.enabled = false;
-
-        ////Incase the wheelcoliders were what the problem with teleporting the car
-        //var wheels = carGO.transform.GetChild(2);
-        //wheels.gameObject.SetActive(false);
-
-        //carRigidbody.transform.position = spawnPosition;
-
         carGO.transform.position = spawnPosition;
+
         //This is the gold mine right here remeber to use this if there are problems with transform.position
         Physics.SyncTransforms();
 
-        //wheels.gameObject.SetActive(true);
-        //colliderGO.enabled = true;
-
-        //carRigidbody.useGravity = true;
-        //carRigidbody.interpolation = RigidbodyInterpolation.Interpolate;
     }
 }
