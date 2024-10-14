e1d29a9f3741872577461eae2a956b9c48ef18a1
theAshamedsleeper <thorbjorn114@gmail.com>
2024-10-14 15:26:49 +0200

    debug removing

--- Assets/Scenes/11-PlayerHouseScene/script/SaveData/HouseLoadSaveController.cs
+++ Assets/Scenes/11-PlayerHouseScene/script/SaveData/HouseLoadSaveController.cs
@@ -33,7 +33,6 @@ namespace Scenes._11_PlayerHouseScene.script.SaveData
             // Use SaveGameController to save the house data to Unity Cloud Save
             await saveGameController.SaveDataAsync(houseDataDTO, HOUSETAG);
 
-            //Debug.Log("House data saved successfully to cloud.");
         }
 
         public async Task<T> LoadGridData<T>() where T : IDataTransferObject
