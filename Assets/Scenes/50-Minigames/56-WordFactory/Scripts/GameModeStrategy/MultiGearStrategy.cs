6eed4de2aca2187dba04fc8a49e3af527e06a0bc
MTBonde <thorbonde@gmail.com>
2024-10-14 12:54:10 +0200

    Update MultiGearStrategy.cs

--- Assets/Scenes/50-Minigames/56-WordFactory/Scripts/GameModeStrategy/MultiGearStrategy.cs
+++ Assets/Scenes/50-Minigames/56-WordFactory/Scripts/GameModeStrategy/MultiGearStrategy.cs
@@ -43,52 +43,87 @@ namespace Scenes._50_Minigames._56_WordFactory.Scripts.GameModeStrategy
                 gearLetters.Add(new List<char>());
             }
 
+            // usedLettersPerGear is a list of HashSets, each containing the letters already used for each gear
+            List<HashSet<char>> usedLettersPerGear = new List<HashSet<char>>();
+            for (int i = 0; i < numberOfGears; i++)
+            {
+                usedLettersPerGear.Add(new HashSet<char>());
+            }
+
             foreach (var word in words)
             {
                 for (int gearIndex = 0; gearIndex < numberOfGears; gearIndex++)
                 {
                     if (gearIndex < word.Length)
                     {
-                        gearLetters[gearIndex].Add(word[gearIndex]);
+                        char letter = word[gearIndex];
+                
+                        // check if the letter is already used
+                        if (!usedLettersPerGear[gearIndex].Contains(letter))
+                        {
+                            gearLetters[gearIndex].Add(letter);
+                            usedLettersPerGear[gearIndex].Add(letter);
+                        }
+                        else
+                        {
+                            // if the letter is already used, add a random letter
+                            char newLetter;
+                            do
+                            {
+                                newLetter = LetterManager.GetRandomLetters(1).First();
+                            } while (usedLettersPerGear[gearIndex].Contains(newLetter));
+
+                            gearLetters[gearIndex].Add(newLetter);
+                            usedLettersPerGear[gearIndex].Add(newLetter);
+                        }
                     }
                     else
                     {
-                        gearLetters[gearIndex].Add(LetterManager.GetRandomLetters(1).First());
+                        // if the word is shorter than the number of gears, add random letters
+                        char newLetter;
+                        do
+                        {
+                            newLetter = LetterManager.GetRandomLetters(1).First();
+                        } while (usedLettersPerGear[gearIndex].Contains(newLetter));
+
+                        gearLetters[gearIndex].Add(newLetter);
+                        usedLettersPerGear[gearIndex].Add(newLetter);
                     }
                 }
             }
 
             return gearLetters;
         }
+
         
         private void FillRemainingLetters(List<List<char>> gearLetters, int numberOfTeeth)
         {
-            // Set to track used letters to avoid duplicates
-            HashSet<char> usedLetters = new HashSet<char>();
-        
-            // Add the already used letters from gearLetters to the set
             foreach (var gear in gearLetters)
             {
-                foreach (var letter in gear)
+                // Create a HashSet and include all letters already assigned from words
+                HashSet<char> usedLettersOnGear = new HashSet<char>(gear);
+                
+                // Add all already assigned letters to the HashSet
+                foreach (char letter in gear)
                 {
-                    usedLetters.Add(letter);
+                    usedLettersOnGear.Add(letter);
                 }
-            }
-        
-            foreach (var gear in gearLetters)
-            {
+
+                // Fill the remaining teeth with unique letters
                 while (gear.Count < numberOfTeeth)
                 {
+                    // Generate a new random letter
                     char newLetter;
+
+                    // Keep trying until we find a unique letter for the current gear
                     do
                     {
-                        // Generate a new letter
                         newLetter = LetterManager.GetRandomLetters(1).First();
-                    } while (usedLetters.Contains(newLetter)); 
-        
-                    // Add the new unique letter to the gear and track it
+                    } while (usedLettersOnGear.Contains(newLetter));
+
+                    // Add the letter to the gear and mark it as used
                     gear.Add(newLetter);
-                    usedLetters.Add(newLetter);
+                    usedLettersOnGear.Add(newLetter); // Track letters for this gear
                 }
             }
         }
